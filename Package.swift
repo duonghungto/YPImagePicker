f707a333aed973842a9ffb1cc1b5ee1ea448e62e
DuongTH.dev <hungduongwp@gmail.com>
2021-09-06 22:13:58 +0700

    Add files via upload

---
+++ Package.swift
@@ -0,0 +1,37 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "YPImagePicker",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v13),
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "YPImagePicker",
+            targets: ["YPImagePicker"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+        .package(name:"SteviaLayout", url: "https://github.com/freshOS/Stevia", .upToNextMajor(from: "4.7.3")),
+        .package(name:"PryntTrimmerView", url: "https://github.com/HHK1/PryntTrimmerView", .upToNextMajor(from: "4.0.2")),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "YPImagePicker",
+            dependencies: [
+                "SteviaLayout",
+                "PryntTrimmerView"
+            ]),
+        .testTarget(
+            name: "YPImagePickerTests",
+            dependencies: ["YPImagePicker"]),
+    ]
+)
