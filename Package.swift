e9361d18729d6fc1965034defaf082f859cee700
DuongTH.dev <hungduongwp@gmail.com>
2021-09-06 22:29:46 +0700

    Update Package.swift
    + update source path

--- Package.swift
+++ Package.swift
@@ -26,9 +26,13 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "YPImagePicker",
+            path: "Source",
             dependencies: [
                 "SteviaLayout",
                 "PryntTrimmerView"
+            ],
+            resources: [
+                .process("Resources"),
             ]),
         .testTarget(
             name: "YPImagePickerTests",
